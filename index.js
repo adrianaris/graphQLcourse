6cfbe827794a177234f59ee9ff8b8d13510f19d3
Adrian Serbanscu <adrian@adrianserbanescu.com>
2022-03-20 13:47:21 +0100

    ex 8.3

--- index.js
+++ index.js
@@ -91,10 +91,17 @@ const typeDefs = gql`
     genres: [String!]!
   }
 
+  type Author {
+    name: String!
+    born: Int
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `
 
@@ -102,7 +109,14 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => {
+      return authors.map(a => 
+        a = { 
+            ...a,
+            bookCount: books.filter(b => b.author === a.name).length
+          })
+    }
   }
 }
 
