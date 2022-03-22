d91d84bfd6c22d6393c815d0857216f3392d3f7b
Adrian Serbanescu <adrain@adrianserbanescu.com>
2022-03-22 18:17:59 +0100

    course

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { useQuery } from '@apollo/client'
 import Persons from './courseComponents/Persons'
 import PersonForm from './courseComponents/PersonForm'
 import { ALL_PERSONS } from './courseComponents/queries'
+import Notify from './courseComponents/Notify'
 
 const App = () => {
+  const [errorMessage, setErrorMessage] = useState(null)
   const result = useQuery(ALL_PERSONS, {
     pollInterval: 2000
   })
@@ -15,10 +17,18 @@ const App = () => {
     return <div>loading...</div>
   }
 
+  const notify = message => {
+    setErrorMessage(message)
+    setTimeout(() => {
+      setErrorMessage(null)
+    }, 10000)
+  }
+
   return (
     <div>
+      <Notify errorMessage={errorMessage} />
       <Persons persons={result.data.allPersons} />
-      <PersonForm />
+      <PersonForm setError={notify} />
     </div>
   );
 }
