42e464f41faed0a3560805f8241d9813c1558f45
Adrian Serbanscu <adrian@adrianserbanescu.com>
2022-03-24 19:18:17 +0100

    ex partial

--- client/src/components/Authors.js
+++ client/src/components/Authors.js
@@ -1,4 +1,7 @@
+import { useState } from 'react'
+
 const Authors = (props) => {
+  const [author, setAuthor] = useState('')
   if (!props.show) {
     return null
   }
@@ -24,6 +27,15 @@ const Authors = (props) => {
           ))}
         </tbody>
       </table>
+      <h2>Set birthyear</h2>
+      <form onSubmit={console.log('test')}>
+        <select value={author} onChange={({ target }) => setAuthor(target.value)}>
+          {authors?.map(a => (
+            <option key={a.name} value={a.born}>{a.name}</option>
+          ))}
+        </select>
+        born <input type='text' value={author} />
+      </form>
     </div>
   )
 }
