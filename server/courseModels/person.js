15bf55e26c1516dd303d0d35892d88b92717ff5c
Adrian Serbanscu <adrian@adrianserbanescu.com>
2022-03-26 12:46:57 +0100

    course

---
+++ server/courseModels/person.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose')
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  phone: {
+    type: String,
+    required: true,
+    minlength: 5
+  },
+  city: {
+    type: String,
+    required: true,
+    minlength: 3
+  }
+})
+
+module.exports = mongoose.model('Person', schema)
